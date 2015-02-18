c48eccd318892091bb19cc343df346d514ddef4a
Leon Gaban <lg209c@att.com>
2015-02-18 16:58:21 -0600

    added index and main angular app file

---
+++ client/components/app-leongaban.js
@@ -0,0 +1,21 @@
+/*global angular*/
+/* =========================================
+--------------------------------------------
+
+	leongaban.com v 8
+	@leongaban | MEAN stack ninjaneer
+
+--------------------------------------------
+============================================ */
+
+(function() { "use strict";
+
+	var app = angular.module('leonApp', [])
+
+	.controller('MainController', ['$scope', function($scope) {
+
+		var vm = $scope;
+
+	}]);
+
+})();
